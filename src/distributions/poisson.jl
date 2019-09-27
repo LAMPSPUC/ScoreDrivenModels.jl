ad59e1df8cb09ec2b6fdd895867472da42586300
Guilherme Bodin <guilherme.b.moraes@gmail.com>
2019-09-27 18:28:29 -0300

    added tests for generic log lik and score

--- src/distributions/poisson.jl
+++ src/distributions/poisson.jl
@@ -1,23 +1,23 @@
 """
 Proof somewhere
 """
-function score(y::T, ::Poisson, param::Vector{T}) where T
+function score(y::Int, ::Type{Poisson}, param::Vector{T}) where T
     return [y/param[1] - 1]
 end
 
 """
 Proof somewhere
 """
-function fisher_information(::Poisson, param::Vector{T}) where T
+function fisher_information(::Type{Poisson}, param::Vector{T}) where T
     return (1/param[1])
 end
 
 """
 Proof somewhere
 """
-function log_likelihood(::Poisson, y::Vector{T}, param::Vector{Vector{T}}, n::Int) where T
+function log_likelihood(::Type{Poisson}, y::Vector{Int}, param::Vector{Vector{T}}, n::Int) where T
     loglik = 0.0
-    for i in n
+    for i in 1:n
         loglik += y[i]*log(param[i][1]) - param[i][1] - lfactorial(y[i])
     end
     return -loglik
@@ -29,10 +29,10 @@ param_tilde_to_param(::Poisson, param_tilde::Vector{T}) where T = param_tilde_to
 jacobian_param_tilde(::Poisson, param_tilde::Vector{T}) where T = Diagonal(jacobian_param_tilde.(ExponentialLink, param_tilde))
 
 # utils
-function update_dist(dist::Poisson, param::Vector{T}) where T
+function update_dist(D::Type{Poisson}, param::Vector{T}) where T
     return Poisson(param[1])
 end 
 
-function num_params(::Type{<:Poisson})
+function num_params(::Type{Poisson})
     return 1
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,9 @@
-push!(LOAD_PATH, "/home/guilhermebodin/Documents/Github/ScoreDrivenModels.jl/src")
 using ScoreDrivenModels, Distributions, LinearAlgebra
 
-using Test, Random
-cd("test")
+using Test, Random, Expectations
+
+const SDM = ScoreDrivenModels
+
 include("test_recursion.jl")
 include("test_estimate.jl")
+include("test_distributions.jl")
