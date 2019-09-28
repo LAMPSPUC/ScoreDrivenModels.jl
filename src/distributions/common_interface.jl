c3f6035df1573cc7455df8f315220a24717ad14d
Guilherme Bodin <guilherme.b.moraes@gmail.com>
2019-09-28 01:56:23 -0300

    start log-normal

--- src/distributions/common_interface.jl
+++ src/distributions/common_interface.jl
@@ -2,15 +2,31 @@
 const DISTS = [
     Normal;
     Beta;
-    Poisson
+    Poisson;
+    LogNormal
 ]
 
 function score(y::T, D::Type{<:Distribution}, param::Vector{T}) where T
-    return error("score not implemented for $(typeof(D)) distribution")
+    return error("score not implemented for $D distribution")
 end
 function score(y::Int, D::Type{<:Distribution}, param::Vector{T}) where T
-    return error("score not implemented for $(typeof(D)) distribution")
+    return error("score not implemented for $D distribution")
 end
 function fisher_information(D::Type{<:Distribution}, param::Vector{T}) where T
-    return error("fisher information not implemented for $(typeof(D)) distribution")
+    return error("fisher_information not implemented for $D distribution")
+end
+function param_to_param_tilde(D::Type{<:Distribution}, param::Vector{T}) where T 
+    return error("param_to_param_tilde not implemented for $D distribution")
+end
+function param_tilde_to_param(D::Type{<:Distribution}, param_tilde::Vector{T}) where T 
+    return error("param_tilde_to_param not implemented for $D distribution")
+end
+function jacobian_param_tilde(D::Type{<:Distribution}, param_tilde::Vector{T}) where T 
+    return error("jacobian_param_tilde not implemented for $D distribution")
+end
+function update_dist(D::Type{<:Distribution}, param::Vector{T}) where T
+    return error("update_dist not implemented for $D distribution")
+end 
+function num_params(D::Type{<:Distribution})
+    return error("num_params not implemented for $D distribution")
 end
