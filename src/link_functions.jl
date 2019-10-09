1c0239d3d21444a116e8923ccfc9eb9033faa716
Guilherme Bodin <32756941+guilhermebodin@users.noreply.github.com>
2019-10-08 23:04:46 -0300

    Update exponential link (#13)
    * update exponential link
    
    * change variable name from offset to lower_bound

--- src/link_functions.jl
+++ src/link_functions.jl
@@ -8,9 +8,9 @@ jacobian_param_tilde(::Type{IdentityLink}, param_tilde::T) where T = one(T)
 
 struct ExponentialLink <: Link end
 
-param_to_param_tilde(::Type{ExponentialLink}, param::T) where T = log(param)
-param_tilde_to_param(::Type{ExponentialLink}, param_tilde::T) where T = exp(param_tilde)
-jacobian_param_tilde(::Type{ExponentialLink}, param_tilde::T) where T = exp(param_tilde)
+param_to_param_tilde(::Type{ExponentialLink}, param::T, lower_bound::T) where T = log(param - lower_bound)
+param_tilde_to_param(::Type{ExponentialLink}, param_tilde::T, lower_bound::T) where T = exp(param_tilde) + lower_bound
+jacobian_param_tilde(::Type{ExponentialLink}, param_tilde::T, lower_bound::T) where T = exp(param_tilde) + lower_bound
 
 struct LogitLink <: Link end
 
