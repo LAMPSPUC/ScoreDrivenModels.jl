7253b15e1616aa1620f6127ffb98a50b8c57f2ae
Guilherme Bodin <32756941+guilhermebodin@users.noreply.github.com>
2019-10-29 16:31:21 -0300

    added pearson residuals (#15)

--- src/diagnostics.jl
+++ src/diagnostics.jl
@@ -1,4 +1,4 @@
-export quantile_residuals
+export quantile_residuals, pearson_residuals
 
 function quantile_residuals(obs::Vector{T}, gas_sarima::GAS_Sarima{D, T}, initial_params::Vector{Vector{T}}) where {D, T}
 
@@ -21,4 +21,19 @@ function quantile_residuals(obs::Vector{T}, gas_sarima::GAS_Sarima{D, T}, initia
     end
 
     return quant_res
+end
+
+function pearson_residuals(obs::Vector{T}, gas_sarima::GAS_Sarima{D, T}, initial_params::Vector{Vector{T}}) where {D, T}
+
+    n = length(obs)
+    len_ini_par = length(initial_params)
+    pearson = Vector{T}(undef, n - len_ini_par)
+    params_fitted = score_driven_recursion(gas_sarima, obs, initial_params)
+
+    for (i, param) in enumerate(params_fitted[len_ini_par + 1:end - 1])
+        dist = update_dist(D, param)
+        pearson[i] = (obs[i + len_ini_par] - mean(dist))/sqrt(var(dist))
+    end
+
+    return pearson
 end
--- test/test_diagnostics.jl
+++ test/test_diagnostics.jl
@@ -1,4 +1,4 @@
-function normality_quantile_residuals(D, n::Int, lags::Int; seed::Int = 10)
+function normality_quantile_and_pearson_residuals(D, n::Int, lags::Int; seed::Int = 10)
     Random.seed!(seed)
 
     gas_sarima = GAS_Sarima(1, 1, D, 0.0)
@@ -8,17 +8,24 @@ function normality_quantile_residuals(D, n::Int, lags::Int; seed::Int = 10)
 
     y, params = simulate(gas_sarima, n)
     quant_res = quantile_residuals(y, gas_sarima, [params[1]])
+    pearson = pearson_residuals(y, gas_sarima, [params[1]])
+
+    # quantile residuals
     jb = JarqueBeraTest(quant_res)
     @test pvalue(jb) >= 0.05
     Ljb = LjungBoxTest(quant_res, lags)
     @test pvalue(Ljb) >= 0.05
+
+    # pearson
+    Ljb = LjungBoxTest(pearson, lags)
+    @test pvalue(Ljb) >= 0.05
     return 
 end
 
-@testset "quantile_residuals" begin
+@testset "quantile and pearson residuals" begin
     n = 1000
     lags = 1
-    normality_quantile_residuals(Beta, n, lags)
-    normality_quantile_residuals(Normal, n, lags)
-    normality_quantile_residuals(LogNormal, n, lags)
+    normality_quantile_and_pearson_residuals(Beta, n, lags)
+    normality_quantile_and_pearson_residuals(Normal, n, lags)
+    normality_quantile_and_pearson_residuals(LogNormal, n, lags)
 end
