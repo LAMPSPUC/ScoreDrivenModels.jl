966b2d8a256c75806d5a891e0656b4a1e8b0941f
guilhermebodin <guilherme.b.moraes@gmail.com>
2019-11-29 18:15:33 -0300

    added logit link

--- test/test_links.jl
+++ test/test_links.jl
@@ -1,15 +1,19 @@
 @testset "Links" begin
     Random.seed!(10)
-    x = rand()
     atol = 1e-7 
     rtol = 1e-7
 
-    link = SDM.IdentityLink
-    @test SDM.link(link, SDM.unlink(link, x)) ≈ x atol = atol rtol = rtol
+    for x in rand(10)
+        link = SDM.IdentityLink
+        @test SDM.link(link, SDM.unlink(link, x)) ≈ x atol = atol rtol = rtol
 
-    link = SDM.LogLink
-    lb = 1.0
-    @test SDM.link(link, SDM.unlink(link, x, lb), lb) ≈ x atol = atol rtol = rtol
-    lb = 0.0
-    @test SDM.link(link, SDM.unlink(link, x, lb), lb) ≈ x atol = atol rtol = rtol
+        link = SDM.LogLink
+        lb = rand()
+        @test SDM.link(link, SDM.unlink(link, x, lb), lb) ≈ x atol = atol rtol = rtol
+
+        link = SDM.LogitLink
+        lb = rand()
+        ub = rand() + lb
+        @test SDM.link(link, SDM.unlink(link, x, lb, ub), lb, ub) ≈ x atol = atol rtol = rtol
+    end
 end
