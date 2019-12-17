8694818475a8517045486aa3a64e74fc780356cb
guilhermebodin <guilherme.b.moraes@gmail.com>
2019-12-17 09:59:40 -0300

    updated garch script

--- examples/garch.jl
+++ examples/garch.jl
@@ -1,7 +1,8 @@
-using ScoreDrivenModels
+using ScoreDrivenModels, Statistics, DelimitedFiles
 
 const SDM = ScoreDrivenModels
 
+# Overwrite link interface to work with Identity links.
 function SDM.link!(param_tilde::Matrix{T}, ::Type{Normal}, param::Matrix{T}, t::Int) where T 
     param_tilde[t, 1] = link(IdentityLink, param[t, 1])
     param_tilde[t, 2] = link(IdentityLink, param[t, 2])
@@ -18,20 +19,17 @@ function SDM.jacobian_link!(aux::AuxiliaryLinAlg{T}, ::Type{Normal}, param::Matr
     return
 end
 
-y = BG96
-using Statistics
-ini = [mean(y) var(y)]
+y = readdlm("./test/data/BG96.csv")[:]
+
+# Evaluate some approximate initial params
+initial_params = [mean(y) var(y)]
+
+# Define upper bounds and lower bounds for parameters
 ub = [1.0; 1.0; 1.0; 1.0]
 lb = [-1.0; 0.0; 0.0; 0.0]
-gas = GAS(1, 1, Normal, 1.0, time_varying_params = [2])
-res = estimate!(gas, BG96; initial_params = ini, opt_method = IPNewton(gas, 50; ub = ub, lb = lb))
-res.coefs
-res.llk
-loglikelihood(f)
-c = coef(f)
 
-using Test
-@test gas.ω[1] - -0.00616637237701241 ≈ 0 atol = 1e-4 rtol = 1e-4
-@test gas.ω[2] - 0.010760592759725487 ≈ 0 atol = 1e-4 rtol = 1e-4
-@test gas.A[1][2, 2] - 0.15341133818189595 ≈ 0 atol = 1e-4 rtol = 1e-4
-@test gas.B[1][2, 2] - (0.15341133818189595 + 0.8058745318161223) ≈ 0 atol = 1e-4 rtol = 1e-4
+# Define a GAS model where only \sigma^2 varys over time
+gas = GAS(1, 1, Normal, 1.0, time_varying_params = [2])
+
+# Estimate the model
+res = estimate!(gas, y; initial_params = initial_params, opt_method = IPNewton(gas, 50; ub = ub, lb = lb))
