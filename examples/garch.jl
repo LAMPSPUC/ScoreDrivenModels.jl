58c9209228caca44b90189615e2deba6a57874e2
guilhermebodin <guilherme.b.moraes@gmail.com>
2019-12-18 20:41:30 -0300

    update GARCH

--- examples/garch.jl
+++ examples/garch.jl
@@ -1,6 +1,11 @@
+push!(LOAD_PATH, "/home/guilhermebodin/Documents/Github/ScoreDrivenModels.jl/src")
 using ScoreDrivenModels, Statistics, DelimitedFiles
 
 const SDM = ScoreDrivenModels
+
+# The GARCH(1, 1) is equivaent to a GAS(1, 1) Normal model with inverse scaling. The models are only equivalent under 
+# no reparametrizations, here this means that the you should fit the model overwriting the default link!, unlink! and
+# jacobian_link! methods for the Normal distribution to use only the IdentityLink.
 
 # Overwrite link interface to work with Identity links.
 function SDM.link!(param_tilde::Matrix{T}, ::Type{Normal}, param::Matrix{T}, t::Int) where T 
@@ -19,17 +24,43 @@ function SDM.jacobian_link!(aux::AuxiliaryLinAlg{T}, ::Type{Normal}, param::Matr
     return
 end
 
+# Data from [Bollerslev and Ghysels (JBES 1996)](https://doi.org/10.2307/1392425). Took from ARCHModels.jl
 y = readdlm("./test/data/BG96.csv")[:]
 
 # Evaluate some approximate initial params
 initial_params = [mean(y) var(y)]
 
-# Define upper bounds and lower bounds for parameters
-ub = [1.0; 1.0; 1.0; 1.0]
-lb = [-1.0; 0.0; 0.0; 0.0]
+# There are 2 possibilities to estimate the model.
+# The first one is to understand that in the GARCH estimation process usually one 
+# defines that the parameters ω, α_0, α_1 and β_1 are constrained. The problem is 
+# that the equivalent GAS model relies that 
+# ω_1 = ω
+# ω_2 = α_0
+# A_1 = α_1
+# B_1 - A_1 = β_1
+# Here there is no easy way of adding a constraint B_1 - A_1 >= 0. An alternative is to define 
+# bounds assuring the upper bound of A_1 is smaller than the lower bound for B_1.
 
-# Define a GAS model where only \sigma^2 varys over time
+# Define upper bounds and lower bounds for parameters ω_1, ω_2, A_1, B_1
+ub = [1.0; 1.0; 0.5; 1.0]
+lb = [-1.0; 0.0; 0.0; 0.5]
+
+# Define a GAS model where only \sigma^2 is varying over time
 gas = GAS(1, 1, Normal, 1.0, time_varying_params = [2])
 
+# Give an initial_point in the interior of the bounds.
+initial_point = [0.0; 0.5; 0.25; 0.75]
+
 # Estimate the model
-res = estimate!(gas, y; initial_params = initial_params, opt_method = IPNewton(gas, 50; ub = ub, lb = lb))
+res = estimate!(gas, y; 
+                                  initial_params = initial_params, 
+                                  opt_method = IPNewton(gas, [initial_point]; ub = ub, lb = lb))
+
+
+# Another way to estimate the model is to rely on luck and give many random initial_points
+# for a non constrained optimization 
+
+gas = GAS(1, 1, Normal, 1.0, time_varying_params = [2])
+res = estimate!(gas, y; 
+                                  initial_params = initial_params, 
+                                  opt_method = NelderMead(gas, 100))
