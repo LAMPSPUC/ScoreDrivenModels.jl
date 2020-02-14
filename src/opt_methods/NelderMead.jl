57534ae857b44c8346029bf4fa2efaa095c6f14f
Guilherme Bodin <32756941+guilhermebodin@users.noreply.github.com>
2020-02-14 20:58:08 +0100

    Update src/opt_methods/NelderMead.jl
    Co-Authored-By: Raphael Saavedra <raphael.saavedra93@gmail.com>

--- src/opt_methods/NelderMead.jl
+++ src/opt_methods/NelderMead.jl
@@ -10,7 +10,7 @@ end
 """
     NelderMead(model::Model, args...; kwargs...)
 
-If an `Int` is provided the method will sample random initial_points and use them as initial 
+If an `Int` is provided the method will sample that many random initial_points and use them as initial 
 points for Optim NelderMead method. If a `Vector{Vector{T}}` is provided it will use them as 
 initial points for Optim NelderMead method.
 """
