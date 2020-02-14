2292bae0541020eb22ea76f863e82ea4d0c3315e
Guilherme Bodin <32756941+guilhermebodin@users.noreply.github.com>
2020-02-14 20:58:14 +0100

    Update src/opt_methods/LBFGS.jl
    Co-Authored-By: Raphael Saavedra <raphael.saavedra93@gmail.com>

--- src/opt_methods/LBFGS.jl
+++ src/opt_methods/LBFGS.jl
@@ -10,7 +10,7 @@ end
 """
     LBFGS(model::Model, args...; kwargs...)
 
-If an `Int` is provided the method will sample random initial_points and use them as 
+If an `Int` is provided the method will sample that many random initial_points and use them as 
 initial points for Optim LBFGS method. If a `Vector{Vector{T}}` is provided it will use them as 
 initial points for Optim LBFGS method.
 """
