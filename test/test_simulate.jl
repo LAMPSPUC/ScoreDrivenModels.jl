eb3cd8a6df79638e5a15992893add4a15cb95110
Raphael Saavedra <raphael.saavedra@invenialabs.co.uk>
2020-02-25 16:39:53 +0000

    Add simulation test

--- test/test_simulate.jl
+++ test/test_simulate.jl
@@ -1,1 +1,27 @@
-#TODO
+@testset "Simulation" begin
+    @testset "LogNormal" begin
+        Random.seed!(123)
+        y = simulate_GAS_1_12(LogNormal, 0.0, 123)
+
+        v = [0.1, 0.1]
+        gas = Model([1, 12], [1, 12], LogNormal, 0.0)
+        gas.ω = v
+        gas.A[1]  = convert(Matrix{Float64}, Diagonal(3*v))
+        gas.A[12] = convert(Matrix{Float64}, Diagonal(-3*v))
+        gas.B[1]  = convert(Matrix{Float64}, Diagonal(3*v))
+        gas.B[12] = convert(Matrix{Float64}, Diagonal(-3*v))
+
+        scenarios = simulate(y, gas, 50, 1000)
+        @test maximum(scenarios) < 1e5
+        @test minimum(scenarios) > 0
+
+        quants = forecast_ci(y, gas, 50)
+        @test all([quants[i, 1] .< quants[i, 2] .< quants[i, 3] for i in 1:50])
+        @test maximum(quants) < 1e3
+        @test minimum(quants) > 1e-4
+
+        forec = forecast(y, gas, 50)
+        @test maximum(forec) < 100
+        @test minimum(forec) > 0.1
+    end
+end
