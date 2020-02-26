7cd7dd0532762d85eb61945e45a6905890495874
Raphael Saavedra <raphael.saavedra93@gmail.com>
2020-02-26 15:56:37 +0000

    Add simulation test (#83)
    * Add simulation test
    
    * Update runtests.jl
    
    * Remove forecast, update forecast_quantiles
    
    * Update test_simulate.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,8 +6,8 @@ include("utils.jl")
 include("test_links.jl")
 include("test_distributions.jl")
 
-# ScoreDrivenModels
 include("test_recursion.jl")
 include("test_initial_params.jl")
 include("test_diagnostics.jl")
 include("test_estimate.jl")
+include("test_simulate.jl")
--- test/test_simulate.jl
+++ test/test_simulate.jl
@@ -1,1 +1,23 @@
-#TODO
+@testset "Simulation" begin
+    @testset "LogNormal" begin
+        Random.seed!(123)
+        y = simulate_GAS_1_12(LogNormal, 0.0, 123)
+
+        v = [0.1, 0.1]
+        gas = Model([1, 12], [1, 12], LogNormal, 0.0)
+        gas.ω = v
+        gas.A[1]  = convert(Matrix{Float64}, Diagonal(3*v))
+        gas.A[12] = convert(Matrix{Float64}, Diagonal(-3*v))
+        gas.B[1]  = convert(Matrix{Float64}, Diagonal(3*v))
+        gas.B[12] = convert(Matrix{Float64}, Diagonal(-3*v))
+
+        scenarios = simulate(y, gas, 50, 1000)
+        @test maximum(scenarios) < 1e5
+        @test minimum(scenarios) > 0
+
+        quants = forecast_quantiles(y, gas, 50)
+        @test all([quants[i, 1] .< quants[i, 2] .< quants[i, 3] for i in 1:50])
+        @test maximum(quants) < 1e3
+        @test minimum(quants) > 1e-4
+    end
+end
