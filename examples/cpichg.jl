2e98ceff7313eecc1c8b4ff092806ea88d3b3fc6
Guilherme Bodin <32756941+guilhermebodin@users.noreply.github.com>
2020-08-24 17:27:55 -0300

    Update examples/cpichg.jl
    Co-authored-by: Raphael Saavedra <raphael.saavedra93@gmail.com>

--- examples/cpichg.jl
+++ examples/cpichg.jl
@@ -2,7 +2,7 @@ using ScoreDrivenModels, DelimitedFiles, Random, Statistics
 Random.seed!(123); 
 y = vec(readdlm("./test/data/cpichg.csv"));
 
-gas = Model(1, 1, TDistLocationScale, 0.0, time_varying_params=[1; 2]);
+gas = Model(1, 1, TDistLocationScale, 0.0, time_varying_params=[1, 2]);
 f = ScoreDrivenModels.fit!(gas, y);
 
 gas = Model(1, 1, TDistLocationScale, 0.0, time_varying_params=[1; 2]);
