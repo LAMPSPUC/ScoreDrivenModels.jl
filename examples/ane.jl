25e971a770fbd2e3889279c05fab17eb65416d5b
Raphael Saavedra <raphael.saavedra@invenialabs.co.uk>
2020-08-25 10:47:18 +0100

    improve examples

--- examples/ane.jl
+++ examples/ane.jl
@@ -1,17 +1,30 @@
-using ScoreDrivenModels, Plots, DelimitedFiles, Dates, Random
+using Dates, DelimitedFiles, Plots, Random, ScoreDrivenModels
 
+# Define dates and load historical Affluent Natural Energy data
 dates = collect(Date(1961):Month(1):Date(2000, 12))
-Random.seed!(123);
-y = vec(readdlm("../test/data/ane_northeastern.csv"));
-y_train = y[1:400];
-gas = Model([1, 2, 11, 12], [1, 2, 11, 12], LogNormal, 0.0; time_varying_params=[1]);
-initial_params = dynamic_initial_params(y_train, gas);
-f = ScoreDrivenModels.fit!(gas, y_train; initial_params=initial_params);
+y = vec(readdlm("../test/data/ane_northeastern.csv"))
+y_train = y[1:400]
+y_test = y[401:460]
+
+# Set RNG seed to guarantee consistent results
+Random.seed!(123)
+
+# Specify GAS model: a lognormal model with time-varying μ, constant σ, and lags 1 and 12
+gas = Model([1, 2, 11, 12], [1, 2, 11, 12], LogNormal, 0.0; time_varying_params=[1])
+
+# Obtain initial parameters to start the GAS recursion
+initial_params = dynamic_initial_params(y_train, gas)
+
+# Fit specified model to historical data using initial parameters
+f = ScoreDrivenModels.fit!(gas, y_train; initial_params=initial_params)
+
+# Get estimation statistics
 estimation_stats = fit_stats(f)
 
-forec = ScoreDrivenModels.forecast(y_train, gas, 60; S=1_000, initial_params=initial_params)
+# Simulate 1000 future scenarios and obtain the 5% and 95% quantiles in each time period
+forec = forecast(y_train, gas, 60; S=1000, initial_params=initial_params)
 
-y_test = y[401:460]
-p2 = plot(dates[401:460], forec.observation_scenarios, color="grey", width=0.05, label="", ylims=(0, 70))
-plot!(p2, dates[360:460], y[360:460], label="ANE", color="black", xlabel="Months", ylabel="GWmed", legend=:topright)
-plot!(p2, dates[401:460], forec.observation_quantiles, label=["Quantiles" "" ""], color="red", line=:dash)
+# Plot results
+plot(dates[401:460], forec.observation_scenarios, color="grey", width=0.05, label="", ylims=(0, 70))
+plot!(dates[360:460], y[360:460], label="ANE", color="black", xlabel="Months", ylabel="GWmed", legend=:topright)
+plot!(dates[401:460], forec.observation_quantiles, label=["Quantiles" "" ""], color="red", line=:dash)
--- examples/cpichg.jl
+++ examples/cpichg.jl
@@ -1,20 +1,39 @@
-using ScoreDrivenModels, DelimitedFiles, Random, Statistics
-Random.seed!(123); 
-y = vec(readdlm("../test/data/cpichg.csv"));
+using DelimitedFiles, Random, ScoreDrivenModels, Statistics
 
-gas = Model(1, 1, TDistLocationScale, 0.0, time_varying_params=[1, 2]);
-f = ScoreDrivenModels.fit!(gas, y);
+# Load historical Consumer Price Index data
+y = vec(readdlm("../test/data/cpichg.csv"))
 
-gas = Model(1, 1, TDistLocationScale, 0.0, time_varying_params=[1, 2]);
-f = fit!(gas, y; verbose=2);
+# Set RNG seed to guarantee consistent results
+Random.seed!(123)
 
-gas = Model(1, 1, TDistLocationScale, 0.0, time_varying_params=[1; 2]);
-f = fit!(gas, y; opt_method=LBFGS(gas, 5));
+# Specify GAS model: a student's t model with location scale transformation
+# (see /src/distributions/non_native_dists.jl in the repository)
+gas = Model(1, 1, TDistLocationScale, 0.0, time_varying_params=[1, 2])
 
+# Fit specified model to historical data
+f = fit!(gas, y)
+
+# Next, we show examples using nondefault keyword arguments
+# Note that we need to re-define `gas` since its parameters were populated by `fit!`
+gas = Model(1, 1, TDistLocationScale, 0.0, time_varying_params=[1, 2])
+
+# In this example, we set verbose to 2, prompting the package to output optimization results
+f = fit!(gas, y; verbose=2)
+
+# Re-define `gas` once again for another example
+gas = Model(1, 1, TDistLocationScale, 0.0, time_varying_params=[1, 2])
+
+# In this example, we set the optimization method to LBFGS with 5 initial points
+f = fit!(gas, y; opt_method=LBFGS(gas, 5))
+
+# Get estimation statistics
 estimation_stats = fit_stats(f)
 
-forec = forecast(y, gas, 12);
-forec.parameter_forecast
-forec.observation_scenarios
+# Perform forecast via simulations for 12 time periods ahead
+forec = forecast(y, gas, 12)
 
-param = score_driven_recursion(gas, y)
+# We can access the parameter forecasts in `forec.parameter_forecasts`
+forec.parameter_forecast
+
+# Similarly, we can access the simulated observation scenarios
+forec.observation_scenarios
