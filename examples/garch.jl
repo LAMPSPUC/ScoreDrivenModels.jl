db66a25211837b4ef6e3728aa3354af9a9323baf
Raphael Saavedra <raphael.saavedra93@gmail.com>
2020-08-25 15:31:57 +0100

    improve examples (#107)

--- examples/garch.jl
+++ examples/garch.jl
@@ -1,6 +1,8 @@
-using ScoreDrivenModels, Statistics, DelimitedFiles
+using DelimitedFiles, ScoreDrivenModels, Statistics
 
-# Overwrite link interface to use IdentityLink for Normal distribution.
+# This is a more advanced example. We want to specify a GARCH model using the GAS framework.
+# To that end, we need to overwrite the default link-unlink interface as it would lead to
+# a different parametrization.
 function ScoreDrivenModels.link!(param_tilde::Matrix{T}, ::Type{Normal}, param::Matrix{T}, t::Int) where T
     param_tilde[t, 1] = link(IdentityLink, param[t, 1])
     param_tilde[t, 2] = link(IdentityLink, param[t, 2])
@@ -17,12 +19,28 @@ function ScoreDrivenModels.jacobian_link!(aux::AuxiliaryLinAlg{T}, ::Type{Normal
     return
 end
 
-y = vec(readdlm("../test/data/BG96.csv"));
-initial_params = [mean(y) var(y)];
-ub = [1.0, 1.0, 0.5, 1.0];
-lb = [-1.0, 0.0, 0.0, 0.5];
-gas = Model(1, 1, Normal, 1.0, time_varying_params = [2]);
-initial_point = [0.0, 0.5, 0.25, 0.75];
-f = fit!(gas, y; initial_params = initial_params,
-                opt_method = IPNewton(gas, [initial_point]; ub = ub, lb = lb));
+# Load (TODO: what is this data?) data
+y = vec(readdlm("../test/data/BG96.csv"))
+
+# Set initial parameters as observed mean and variance of the series
+initial_params = [mean(y) var(y)]
+
+# We can set upper and lower bounds for the parameters
+ub = [1.0, 1.0, 0.5, 1.0]
+lb = [-1.0, 0.0, 0.0, 0.5]
+
+# Specify GAS model: a normal model with time-varying σ and lag 1
+gas = Model(1, 1, Normal, 1.0, time_varying_params = [2])
+
+# Set initial point for the optimization
+initial_point = [0.0, 0.5, 0.25, 0.75]
+
+# Fit specified model to historical data
+f = fit!(
+    gas, y;
+    initial_params = initial_params,
+    opt_method = IPNewton(gas, [initial_point]; ub=ub, lb=lb)
+)
+
+# Get estimation statistics
 estimation_stats = fit_stats(f)
