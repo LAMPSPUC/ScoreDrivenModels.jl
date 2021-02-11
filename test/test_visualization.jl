b4977f73d694f3f7140ae59a0a0b8fd245298d6a
Guilherme Bodin <guilherme.b.moraes@gmail.com>
2021-02-10 22:12:06 -0300

    update main file and tests

--- test/test_visualization.jl
+++ test/test_visualization.jl
@@ -1,4 +1,4 @@
-@testset "Visualization Residuals" begin
+@testset "Visualizations" begin
     ω = [0.1, 0.1]
     A = [0.5 0; 0 0.5]
     B = [0.5 0; 0 0.5]
@@ -8,4 +8,12 @@
     f = fit!(gas, simulation; verbose = 2, opt_method = ScoreDrivenModels.LBFGS(gas, 3))
     rec = RecipesBase.apply_recipe(Dict{Symbol, Any}(), f)
     @test length(rec) == 7
+    ω = [0.1, 0.1]
+    A = [0.5 0; 0 0.5]
+    B = [0.5 0; 0 0.5]
+    simulation = simulate_GAS_1_1(Normal, 0.0, ω, A, B, 1)
+    gas = ScoreDrivenModels.Model(1, 1, Normal, 0.0)
+    bac = backtest(gas, simulation, 10, 4985)
+    rec = RecipesBase.apply_recipe(Dict{Symbol, Any}(), bac, "name")
+    @test length(rec) == 2
 end
