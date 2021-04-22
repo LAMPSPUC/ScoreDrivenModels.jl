2ac73441927312e9343e8169c715e4ba98e2ccc0
Raphael Saavedra <raphael.saavedra93@gmail.com>
2021-04-22 16:49:40 +0100

    Fix fitted_mean docstring

--- src/univariate_score_driven_recursion.jl
+++ src/univariate_score_driven_recursion.jl
@@ -127,9 +127,11 @@ function update_param_tilde!(param_tilde::Matrix{T}, ω::Vector{T}, A::Dict{Int,
 end
 
 """
-    fitted_mean(gas::Model{D, T}, observations::Vector{T}, initial_params::Vector{T}) where {D, T}
+    fitted_mean(gas::Model{D, T}, observations::Vector{T}; 
+                initial_params::Matrix{T}=stationary_initial_params(gas)) where {D, T}
 
-return the fitted mean.. #TODO
+Returns the fitted mean of the in-sample series, i.e., the mean of the predictive 
+distribution at each time step using the fitted parameters in `gas`.
 """
 function fitted_mean(gas::Model{D, T}, observations::Vector{T}; 
                      initial_params::Matrix{T} = stationary_initial_params(gas)) where {D, T}
